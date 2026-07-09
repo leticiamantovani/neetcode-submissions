c7c7a16f9cecd0533372b26a998f09eeede95f23
Leticia Mantovani <68133834+leticiamantovani@users.noreply.github.com>
2026-07-08 22:58:07 -0300

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        checked = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in checked:
+                return [checked[diff], i]
+            else: 
+                checked[nums[i]] = i
+        
